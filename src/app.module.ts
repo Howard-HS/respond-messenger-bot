3705a05e9d16af1666de5caf79da642e601dbf03
Howard Chang <hschang.2008@gmail.com>
2021-08-12 23:16:47 +0800

    Add boilerplate codes for modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WebhookModule } from './webhook/webhook.module';
+import { MessageModule } from './message/message.module';
+import { ProductModule } from './product/product.module';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [WebhookModule],
+  imports: [WebhookModule, MessageModule, ProductModule, EmailModule],
   controllers: [AppController],
   providers: [AppService],
 })

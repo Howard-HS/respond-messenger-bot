2f254eda1550370b7a424bc81ec02b5473fb4ccb
Howard Chang <hschang.2008@gmail.com>
2021-08-15 01:06:59 +0800

    Remove unused webhook service

--- src/webhook/webhook.module.ts
+++ src/webhook/webhook.module.ts
@@ -4,7 +4,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { EmailService } from '../email/email.service';
 import { MessageService } from '../message/message.service';
 import { ProductService } from '../product/product.service';
-import { WebhookService } from './webhook.service';
 import { CustomerService } from '../customer/customer.service';
 
 import { WebhookController } from './webhook.controller';
@@ -14,12 +13,6 @@ import { Customer } from '../customer/model/customer.entity';
 @Module({
   imports: [TypeOrmModule.forFeature([Product, Customer])],
   controllers: [WebhookController],
-  providers: [
-    WebhookService,
-    MessageService,
-    EmailService,
-    ProductService,
-    CustomerService,
-  ],
+  providers: [MessageService, EmailService, ProductService, CustomerService],
 })
 export class WebhookModule {}

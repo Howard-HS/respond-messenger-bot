b67d05f915217c12365e16a8683e12d648f35b2e
Howard Chang <hschang.2008@gmail.com>
2021-08-15 16:12:27 +0800

    Wrap verify payload guard into generic try-catch

--- src/webhook/guards/verify-payload.guard.ts
+++ src/webhook/guards/verify-payload.guard.ts
@@ -15,25 +15,24 @@ export class VerifyPayload implements CanActivate {
   canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest() as Request;
 
-    if (!request.body) {
-      throw new BadRequestException('PAYLOAD_EMPTY');
-    }
+    try {
+      const rawData = Buffer.from(request.body).toString('utf8');
+      const incomingSignature = request.headers['x-hub-signature'] as string;
 
-    const rawData = Buffer.from(request.body).toString('utf8');
-    const incomingSignature = request.headers['x-hub-signature'] as string;
+      if (
+        incomingSignature &&
+        incomingSignature.length === 45 &&
+        incomingSignature.substring(0, 5) === 'sha1='
+      ) {
+        const appSecret = this.configService.get('APP_SECRET');
+        const signature = createHmac('sha1', appSecret)
+          .update(rawData)
+          .digest('hex');
 
-    if (
-      incomingSignature &&
-      incomingSignature.length === 45 &&
-      incomingSignature.substring(0, 5) === 'sha1='
-    ) {
-      const signature = createHmac('sha1', this.configService.get('APP_SECRET'))
-        .update(rawData)
-        .digest('hex');
-
-      return signature === incomingSignature.substring(5);
-    } else {
-      throw new BadRequestException('INVALID_SIGNATURE');
+        return signature === incomingSignature.substring(5);
+      }
+    } catch (error) {
+      throw new BadRequestException('PAYLOAD_OR_SIGNATURE_INVALID');
     }
   }
 }
